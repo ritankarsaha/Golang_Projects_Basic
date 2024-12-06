e9136c8088da862530e0d96ac8a1a94614ce6e7f
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-06 12:28:17 +0530

    final commit

--- go-postgres/models/books.go
+++ go-postgres/models/books.go
@@ -2,14 +2,14 @@ package models
 
 import "gorm.io/gorm"
 
-type Book struct {
+type Books struct {
 	ID        uint   `json:"id" gorm:"primary_key;autoIncrement"`
 	Title     string `json:"title"`
 	Author    string `json:"author"`
 	Publisher string `json:"publisher"`
 }
 
-func Migration(db *gorm.DB) error {
-	err := db.AutoMigrate(&Book{})
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
 	return err
 }
